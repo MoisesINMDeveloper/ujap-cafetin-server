7b07798164de1d1940cba45b97bdf9918753d80c
MoisesINMDeveloper <moisesisraelnavarro@gmail.com>
2025-01-20 11:08:42 -0400

    update

--- src/routes/categories/category.routes.ts
+++ src/routes/categories/category.routes.ts
@@ -10,11 +10,7 @@ import { authenticatedReq } from '../../middleware/auth.middleware';
 
 const router = Router();
 router.post('/', authenticatedReq, createCategoryController);
-<<<<<<< HEAD
 router.get('/all-categories', getCategoriesController);
-=======
-router.get('/all-categories', authenticatedReq, getCategoriesController);
->>>>>>> d007b5b992be42dd50c525f5347fba5fa48acee7
 router.get('/:id', authenticatedReq, getCategoryController);
 router.put('/', authenticatedReq, updateCategoryController);
 router.delete('/:id', authenticatedReq, deleteCategoryController);
